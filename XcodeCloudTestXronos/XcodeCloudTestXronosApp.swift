f155a1104ebda7e58636dae4cb54f6e36ccac624
h.jang <h.jang@xronos-inc.jp>
2024-11-06 11:00:00 +0900

    Initial Commit

---
+++ XcodeCloudTestXronos/XcodeCloudTestXronosApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestXronosApp.swift
+//  XcodeCloudTestXronos
+//
+//  Created by ジャン ヒョンジュン on 2024/11/06.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudTestXronosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XcodeCloudTestXronosTests/XcodeCloudTestXronosTests.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestXronosTests.swift
+//  XcodeCloudTestXronosTests
+//
+//  Created by ジャン ヒョンジュン on 2024/11/06.
+//
+
+import Testing
+@testable import XcodeCloudTestXronos
+
+struct XcodeCloudTestXronosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
